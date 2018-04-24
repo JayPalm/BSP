5b7ba3b67d650a89506afa0ecebd76fd7e487ea4
Justin Palmer <jp@Justins-MacBook-Pro-2.local>
2018-04-24 14:50:50 -0700

    sweep maker

--- BSS_data.py
+++ BSS_data.py
@@ -26,3 +26,18 @@ for index,row in dS.iterrows():
 		optout = row["Optout"] if not pd.isnull(row["Optout"]) else 0
 	)
 	sweep.save()
+
+row =dS.loc[3]
+sweep = Sweep.objects.create(
+rte = row["RTE"],
+street_name = row["Street Name"],
+from_address = row["From Address"],
+to_address = row["To Address"],
+day = Weekday.objects.get(num=int(str(row['RTE'])[0])),
+of_month = int(str(row['RTE'])[1]),
+time = row["Time"],
+side = row["Side"],
+from_street = row["From Street"],
+to_street = row["To Street"],
+optout = row["Optout"] if not pd.isnull(row["Optout"]) else 0
+)
